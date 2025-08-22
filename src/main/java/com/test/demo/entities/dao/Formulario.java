724e6fd4817b2cd4094b550c214f4f4c5ef8a0ac
Hector Geovanny Rodriguez Martinez <hector.rodriguez@oncontrol.com.mx>
2025-08-22 13:55:17 -0600

    commit 5

--- src/main/java/com/test/demo/entities/dao/Formulario.java
+++
@@ -1,9 +0,0 @@
-package com.test.demo.entities.dao;
-
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface Formulario extends JpaRepository<Formulario, UUID> {
-}
